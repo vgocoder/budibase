4b04509c3f523602f8f5d4f4e7b34001a9147294
Adria Navarro <adria@budibase.com>
2025-04-25 14:51:10 +0200

    Add prompt

--- packages/builder/src/pages/builder/app/[application]/design/_components/NewScreen/AIScreenGeneration.svelte
+++ packages/builder/src/pages/builder/app/[application]/design/_components/NewScreen/AIScreenGeneration.svelte
@@ -26,6 +26,7 @@
     "Create me a basic registry form",
     "Create me a form for the courses table",
     "A dashboard to monitorise the jobs datasource",
+    "Two pie charts next to each other (in a row)",
   ]
 </script>
 
